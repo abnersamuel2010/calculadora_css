72be520fe301c28c1a643973158c36e72a01de1e
abnersamuel2010 <tomaz.abner@escola.pr.gov.br>
2025-06-25 23:03:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,12 @@ function soma(){
 }
 function subtrair(){
 
+    let numero_um = parseInt(document.getElementById('primeiro_numero').value);
+    let numero_dois = parseInt(document.getElementById('segundo_numero').value);
+
+    let subtrair = numero_um - numero_dois;
+
+    document.getElementById('resultado').textContent = 'Resultado: '+ subtrair;
+
 
 }

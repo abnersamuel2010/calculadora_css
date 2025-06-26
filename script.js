8d48a9697ddab14f9817b22161889f8414bb8285
abnersamuel2010 <tomaz.abner@escola.pr.gov.br>
2025-06-26 08:38:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,46 @@ function subtrair(){
 
 
 }
+function multiplicação(){
+
+    let numero_um = parseInt(document.getElementById('primeiro_numero').value);
+    let numero_dois = parseInt(document.getElementById('segundo_numero').value);
+
+    let multiplicação = numero_um * numero_dois;
+
+    document.getElementById('resultado').textContent = 'Resultado: '+ multiplicação;
+
+
+
+
+}
+function divisão(){    
+
+    let numero_um = parseFloat(document.getElementById('primeiro_numero').value);
+    let numero_dois = parseFloat(document.getElementById('segundo_numero').value);
+
+    if( numero_dois == 0 ){
+
+      alert( "Não é possivel dividir!!" );
+      return;
+    }
+
+    let divisão = numero_um / numero_dois;
+
+    document.getElementById('resultado').textContent = 'Resultado: '+ divisão;
+
+}
+
+function comparação(){
+    let numero_um = parseFloat(document.getElementById('primeiro_numero').value);
+    let numero_dois = parseFloat(document.getElementById('segundo_numero').value);
+    if(numero_um > numero_dois ){
+        document.getElementById('resultado').textContent = "O maior é " + numero_um;
+        return;
+    }else if(numero_um < numero_dois){
+        document.getElementById('resultado').textContent = "O maior é " + numero_dois;
+        return;
+    }else{
+        document.getElementById('resultado').textContent = "São iguais ";
+    }
+}
